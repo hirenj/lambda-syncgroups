181a9cdd1a4f8b83530a8f76ecc6bf447640aed2
Hiren Joshi <hirenj@gmail.com>
2016-03-12 00:07:55 +0100

    Stub for watch changes

---
+++ watch_changes.js
@@ -0,0 +1,9 @@
+// POST https://www.googleapis.com/drive/v3/changes/watch
+
+// Subscribe for user
+
+// Run on an api call, push changed files onto an SNS topic
+// respondToWebhook
+
+// Run on a timer
+// renewWebhook
