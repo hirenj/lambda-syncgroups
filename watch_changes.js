89678960d4a05fb1c21842fdc871bc7a4ac60f37
Hiren Joshi <hirenj@gmail.com>
2016-05-27 21:30:15 +0200

    Directory cleanup

--- watch_changes.js
+++
@@ -1,9 +0,0 @@
-// POST https://www.googleapis.com/drive/v3/changes/watch
-
-// Subscribe for user
-
-// Run on an api call, push changed files onto an SNS topic
-// respondToWebhook
-
-// Run on a timer
-// renewWebhook
