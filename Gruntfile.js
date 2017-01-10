7cd8301e5977545175fed60a473a49b8eed8cfa1
Hiren Joshi <hirenj@gmail.com>
2017-01-11 00:45:54 +0100

    Add in generic gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,116 +4,59 @@
  */
 'use strict';
 module.exports = function(grunt) {
-	require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	var path = require('path');
+  var path = require('path');
 
-	var config = {'functions' : {} };
-	try {
-		config = require('./resources.conf.json');
-	} catch (e) {
-	}
+  var config = {'functions' : {} };
+  try {
+    config = require('./resources.conf.json');
+  } catch (e) {
+  }
 
-	grunt.initConfig({
-		lambda_invoke: {
-			default: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'populateGroupGrants',
-					event: 'event.json',
-				},
-			},
-		},
-		lambda_deploy: {
-			populateGroupGrants: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.populateGroupGrants',
-					region: config.region,
-				},
-				function: config.functions['populateGroupGrants'] || 'populateGroupGrants',
-				arn: null,
-			},
-			downloadFile: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.downloadFile',
-					region: config.region,
-				},
-				function: config.functions['downloadFile'] || 'downloadFile',
-				arn: null,
-			},
-			downloadFiles: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.downloadFiles',
-					region: config.region,
-				},
-				function: config.functions['downloadFiles'] || 'downloadFiles',
-				arn: null,
-			},
-			downloadEverything: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.downloadEverything',
-					region: config.region,
-				},
-				function: config.functions['downloadEverything'] || 'downloadEverything',
-				arn: null,
-			},
-			subscribeWebhook: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.subscribeWebhook',
-					region: config.region,
-				},
-				function: config.functions['subscribeWebhook'] || 'subscribeWebhook',
-				arn: null,
-			},
-			acceptWebhook: {
-				package: 'syncgroups',
-				options: {
-					file_name: 'index.js',
-					handler: 'index.acceptWebhook',
-					region: config.region,
-				},
-				function: config.functions['acceptWebhook'] || 'acceptWebhook',
-				arn: null,
-			},
-		},
-		lambda_package: {
-			populateGroupGrants: {
-				package: 'syncgroups',
-			},
-			downloadFile: {
-				package: 'syncgroups',
-			},
-			downloadFiles: {
-				package: 'syncgroups',
-			},
-			downloadEverything: {
-				package: 'syncgroups',
-			},
-			subscribeWebhook: {
-				package: 'syncgroups',
-			},
-			acceptWebhook: {
-				package: 'syncgroups',
-			}
-		},
-		env: {
-			prod: {
-				NODE_ENV: 'production',
-			},
-		},
+  process.env['AWS_REGION'] = config.region;
 
-	});
+  var self_funcs = Object.keys(require('./index')).filter( (func) => config.functions[func] );
+
+  var config_options = {
+    'git-describe': {
+      options: {},
+      default: {}
+    },
+    lambda_package: {
+      common: {
+        package: 'package'
+      }
+    },
+    env: {
+      prod: {
+        NODE_ENV: 'production',
+      },
+    }
+
+  };
+  config_options['lambda_deploy'] = {};
+
+  self_funcs.forEach( (funcname) => {
+    config_options['lambda_deploy'][funcname] = {
+        options: {
+          file_name: 'index.js',
+          handler: 'index.'+funcname,
+          region: config.region,
+        },
+        function: config.functions[funcname] || funcname,
+        arn: null,
+    };
+  });
+
+  config_options['lambda_checkversion'] = config_options['lambda_setversion'] = config_options['lambda_package_targets'] = {};
+  Object.keys(config_options['lambda_deploy']).forEach(function(targ) {
+    config_options['lambda_checkversion'][targ] = true;
+    config_options['lambda_setversion'][targ] = true;
+    config_options['lambda_package_targets'][targ] = true;
+  });
+
+  grunt.initConfig(config_options);
 
 	grunt.registerTask('authorise','Populate authorisation',function() {
 		var done = this.async();
@@ -199,12 +142,102 @@ module.exports = function(grunt) {
 	});
 
 	grunt.registerTask('bootstrap' , ['authorise', 'encrypt-secrets-aws']);
-	grunt.registerTask('deploy:populateGroupGrants', ['env:prod', 'lambda_package:populateGroupGrants', 'lambda_deploy:populateGroupGrants']);
-	grunt.registerTask('deploy:downloadFile', ['env:prod', 'lambda_package:downloadFile', 'lambda_deploy:downloadFile']);
-	grunt.registerTask('deploy:downloadFiles', ['env:prod', 'lambda_package:downloadFiles', 'lambda_deploy:downloadFiles']);
-	grunt.registerTask('deploy:downloadEverything', ['env:prod', 'lambda_package:downloadEverything', 'lambda_deploy:downloadEverything']);
-	grunt.registerTask('deploy:subscribeWebhook', ['env:prod', 'lambda_package:subscribeWebhook', 'lambda_deploy:subscribeWebhook']);
-	grunt.registerTask('deploy:acceptWebhook', ['env:prod', 'lambda_package:acceptWebhook', 'lambda_deploy:acceptWebhook']);
-	grunt.registerTask('deploy', ['env:prod', 'lambda_package', 'lambda_deploy']);
+
+
+
+  grunt.registerTask('saveRevision', function() {
+
+      var branch = 'none';
+      var done = this.async();
+
+      grunt.event.once('git-describe', function (rev) {
+        grunt.option('gitRevision', branch+'-'+rev);
+      });
+
+      grunt.util.spawn({
+        cmd: 'git',
+        args: ['symbolic-ref','--short','HEAD']
+      }, function (error, result) {
+          if (error) {
+            grunt.log.error([error]);
+          }
+          branch = result;
+          grunt.task.run('git-describe');
+          done();
+      });
+
+  });
+
+  grunt.registerMultiTask('lambda_package_targets','Check for version of lambda function',function(){
+    if ( ! grunt.option('packagepath') ) {
+      grunt.option('packagepath',grunt.config('lambda_deploy.common.package'));
+      grunt.config('lambda_deploy.common',null);
+    }
+    if (grunt.config('lambda_deploy.'+this.target)) {
+      grunt.config('lambda_deploy.'+this.target+'.package',grunt.option('packagepath'));
+    }
+  });
+
+  grunt.registerMultiTask('lambda_checkversion','Check for version of lambda function',function(){
+    grunt.task.requires('git-describe');
+    var done = this.async();
+    var target = this.target;
+    if ( ! grunt.config().lambda_deploy[target] ) {
+      grunt.log.writeln("No arn");
+      grunt.config('lambda_package.'+target,null);
+      grunt.config('lambda_deploy.'+target,null);
+      grunt.config('lambda_setversion.'+target,null);
+      done();
+      return;
+    }
+    var arn = grunt.config().lambda_deploy[target].function;
+    var AWS = require('aws-sdk');
+    var lambda = new AWS.Lambda();
+    lambda.getFunctionConfiguration({FunctionName: arn},function(err,data) {
+      var git_status = grunt.option('gitRevision');
+      if (git_status.dirty) {
+        grunt.log.writeln("Git repo is dirty, updating by default");
+      } else {
+        var current_version = data.Description;
+        if (current_version === git_status.toString()) {
+          grunt.config('lambda_package.'+target,null);
+          grunt.config('lambda_deploy.'+target,null);
+          grunt.config('lambda_setversion.'+target,null);
+        }
+      }
+      done();
+    });
+  });
+
+  grunt.registerMultiTask('lambda_setversion','Set version for lambda function',function(){
+    grunt.task.requires('git-describe');
+    var done = this.async();
+    var target = this.target;
+    if ( ! grunt.config().lambda_deploy[target] ) {
+      grunt.log.writeln("No arn");
+      done();
+      return;
+    }
+    var arn = grunt.config().lambda_deploy[target].function;
+    var AWS = require('aws-sdk');
+    var lambda = new AWS.Lambda();
+    grunt.log.writeln("Setting version for "+target+" to ",grunt.option('gitRevision').toString());
+    lambda.updateFunctionConfiguration({FunctionName: arn,Description: grunt.option('gitRevision').toString() },function(err,data) {
+      if ( ! err ) {
+        done();
+      } else {
+        grunt.fail.fatal(err);
+      }
+    });
+  });
+
+  grunt.registerTask('versioncheck',['saveRevision']);
+
+  grunt.registerTask('deploy', function(func) {
+    var tasks = ['lambda_deploy','lambda_setversion'];
+    func = (func ? (':' + func) : '');
+    grunt.task.run( ['env:prod','versioncheck','lambda_checkversion'+func,'force:lambda_package','force:lambda_package_targets' ].concat(tasks.map( (task) => 'force:'+task+func) ));
+  });
+
 	grunt.registerTask('test', ['lambda_invoke']);
 };
